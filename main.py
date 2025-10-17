b9f1fabda357923779bad5d632e784165a2bcd38
Seokmin Yun <giggssss@users.noreply.github.com>
2025-10-17 09:49:57 +0900

    modi main online

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print("It demonstrates basic print functionality.")
 print("You can run this script in any Python environment.")
 print("Feel free to modify and expand upon this code!")
 print("Happy coding!")
+
+print('Update')
