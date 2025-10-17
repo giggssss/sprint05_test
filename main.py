37e672907316741001a76b274fa09f8e2964f0c7
윤석민 <ysm861230@gmail.com>
2025-10-17 09:43:08 +0900

    main.py에 추가적인 출력 문구 추가

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 print("Hello, World!")
 print("This is a sample Python script.")
 print("It demonstrates basic print functionality.")
+print("You can run this script in any Python environment.")
+print("Feel free to modify and expand upon this code!")
+print("Happy coding!")
